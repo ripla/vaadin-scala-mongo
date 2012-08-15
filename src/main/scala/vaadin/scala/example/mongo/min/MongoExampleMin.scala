316f254e160980ad159b002659a763023ef54a35
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-08-15 12:40:37 -0400

    Fix form commit and error messages

--- src/main/scala/vaadin/scala/example/mongo/min/MongoExampleMin.scala
+++ src/main/scala/vaadin/scala/example/mongo/min/MongoExampleMin.scala
@@ -1,13 +1,13 @@
-package vaadin.scala.example.mongo
+package vaadin.scala.example.mongo.min
 
 import vaadin.scala._
 import com.mongodb.casbah.Imports._
-import com.novus.salat._
+import com.novus.salat.grater
 import com.novus.salat.global._
 import scala.util.Random
 import scala.reflect.BeanProperty
 
-class MongoExampleMinimal extends Application("Mongo & Vaadin, tied together with Scala") {
+class MongoExampleMin extends Application("Mongo & Vaadin, tied together with Scala") {
 
   val registrations: MongoCollection = MongoConnection()("vaadin-scala-mongo-example")("registrations")
 
@@ -37,14 +37,12 @@ class MongoExampleMinimal extends Application("Mongo & Vaadin, tied together wit
       size(50 pct, 50 pct)
       caption = "Registration"
       formFieldFactory = createFormFieldFactory
-    }
-
-    form.footer = new HorizontalLayout {
-      components += Button("Save", showList)
+      footer = new HorizontalLayout {
+        components += Button("Save", showList)
+      }
     }
 
     components += tableLayout
-
     alignment(tableLayout -> Alignment.MiddleCenter)
 
     def showForm(): Unit = {
@@ -55,29 +53,36 @@ class MongoExampleMinimal extends Application("Mongo & Vaadin, tied together wit
     }
 
     def showList(): Unit = {
-      form.commit
-      val bean = form.item.get.asInstanceOf[BeanItem[MinRegistration]].bean
-      registrations.save(grater[MinRegistration].asDBObject(bean))
-      tableLayout.table.container = new BeanItemContainer[MinRegistration](mapRegistrations)
-      tableLayout.table.visibleColumns = Seq("username", "realName")
-      replaceComponent(form, tableLayout)
-      alignment(tableLayout -> Alignment.MiddleCenter)
-      mainWindow.showNotification("User %s registered".format(bean.username))
+      try {
+        form.commit
+        val bean = form.item.get.asInstanceOf[BeanItem[MinRegistration]].bean
+        registrations += grater[MinRegistration].asDBObject(bean)
+        tableLayout.table.container = new BeanItemContainer[MinRegistration](mapRegistrations)
+        tableLayout.table.visibleColumns = Seq("username", "realName")
+        replaceComponent(form, tableLayout)
+        alignment(tableLayout -> Alignment.MiddleCenter)
+        mainWindow.showNotification("User %s registered".format(bean.username))
+      } catch { case _ => } //form handles error
     }
   }
 
-  def createFormFieldFactory = FormFieldFactory(_ match {
-    case FormFieldIngredients(_, "password", _) =>
-      Some(new PasswordField {
-        caption = DefaultFieldFactory.createCaptionByPropertyId("password")
-        required = true
-      })
+  def createFormFieldFactory = FormFieldFactory(ing => {
+    var field: Option[Field] = None
 
-    case otherIngredient => {
-      val field = DefaultFieldFactory.createField(otherIngredient)
-      field.foreach(_.required = true)
-      field
+    ing match {
+      case FormFieldIngredients(_, "password", _) =>
+        field = Some(new PasswordField {
+          caption = DefaultFieldFactory.createCaptionByPropertyId("password")
+        })
+
+      case otherIngredient => {
+        field = DefaultFieldFactory.createField(otherIngredient)
+      }
     }
+
+    field.foreach(_.required = true)
+    field.foreach(f => f.requiredError_=("%s is required".format(f.caption.get)))
+    field
   })
 }
 
